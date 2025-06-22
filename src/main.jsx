cdbc432e63966451f9f0a31ee42fc5070f527ddb
sari705 <sari125705@gmail.com>
2025-06-22 20:08:10 +0300

    Refine MUI styling

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
 import './index.css'
 import App from './App.jsx'
 
+const theme = createTheme({
+  direction: 'rtl',
+  palette: {
+    primary: {
+      main: '#2b4a6f',
+      light: '#e0eafc',
+    },
+    secondary: {
+      main: '#22334a',
+    },
+    background: {
+      paper: '#fff',
+    },
+  },
+})
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
